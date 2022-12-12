0b4ff83eddc090c9bc231ee5aad3f77995e283dc
Matteo-Vairani <matteo.vairani@student.supsi.ch>
2022-12-12 09:14:36 +0100

    Add html code and Vairani jupyter file

---
+++ html/test.js
@@ -0,0 +1,10 @@
+function test() {
+    var max_translation = document.querySelector(".slider-rail-touch-rect").offsetWidth - 20;
+    document.querySelector(".slider-grip-rect").transform = "translateX(100px)";
+
+    //var iframe = document.querySelector("#includeContent");
+    //if (iframe && iframe.contentWindow) {
+    //iframe.contentWindow.querySelector(".slider-grip-rect").style.transform = "translateX(100%)";
+    //}
+}/*slider-grip-rect*/
+
